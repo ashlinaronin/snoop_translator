0b82254912c9a52b78ec09fa445953914b2d8aaf
Ashlin Aronin & Mitch Rivet <student@epicodus.com>
2015-08-10 14:56:07 -0700

    Add single s ending test and pass all tests

---
+++ tests/SnoopTranslatorTest.php
@@ -0,0 +1,34 @@
+<?php
+
+    require_once "src/SnoopTranslator.php";
+
+    class SnoopTranslatorTest extends PHPUnit_Framework_TestCase
+    {
+
+        function test_snoopTranslate_endingS()
+        {
+            //Arrange
+            $test_SnoopTranslator = new SnoopTranslator;
+            $input = "homies";
+
+            //Act
+            $result = $test_SnoopTranslator->snoopTranslate($input);
+
+            //Assert
+            $this->assertEquals("homiez", $result);
+        }
+
+        function test_snoopTranslate_multipleEndingS()
+        {
+            //Arrange
+            $test_SnoopTranslator = new SnoopTranslator;
+            $input = "boys in the cupboards";
+
+            //Act
+            $result = $test_SnoopTranslator->snoopTranslate($input);
+
+            //Assert
+            $this->assertEquals("boyz in the cupboardz", $result);
+
+        }
+    }
